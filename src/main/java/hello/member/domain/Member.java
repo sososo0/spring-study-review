2e27ee5f2becffd84e468504ada0ab4210509c04
sososo0 <sh0000@pusan.ac.kr>
2023-12-05 17:32:39 +0900

    fix: Modify member id to atomicLong

--- src/main/java/hello/member/domain/Member.java
+++ src/main/java/hello/member/domain/Member.java
@@ -1,17 +1,20 @@
 package hello.member.domain;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Member {
+    private static final AtomicLong idCounter = new AtomicLong(0);
     private Long id;
     private String name;
     private int age;
 
     public Member(String name, int age) {
+        this.id = idCounter.incrementAndGet();
         this.name = name;
         this.age = age;
     }
 
     public Long getId() {
-        id++;
         return id;
     }
 
