aafbe672abf9047d78b3a55766f480380cc2547b
sososo0 <sh0000@pusan.ac.kr>
2023-12-05 17:22:27 +0900

    feat: Add member service to post request

---
+++ src/main/java/hello/member/service/MemberService.java
@@ -0,0 +1,29 @@
+package hello.member.service;
+
+import hello.member.domain.Member;
+import hello.member.dto.request.MemberRequest;
+import hello.member.dto.response.MemberResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberService {
+
+    List<Member> members;
+
+    public MemberResponse.createMemberResponseDto saveMember(
+            MemberRequest.createMemberRequestDto request
+    ) {
+        return new MemberResponse.createMemberResponseDto(addMember(request));
+    }
+
+    private Member addMember(
+            MemberRequest.createMemberRequestDto request
+    ) {
+        Member member = request.toEntity();
+        members.add(member);
+        return member;
+    }
+
+}
