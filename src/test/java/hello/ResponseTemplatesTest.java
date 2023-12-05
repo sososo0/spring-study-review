748d408bd085774969c76b1f1f3ccc4d5fe93c81
sososo0 <sh0000@pusan.ac.kr>
2023-12-05 14:40:15 +0900

    test: Add template test code

---
+++ src/test/java/hello/ResponseTemplatesTest.java
@@ -0,0 +1,22 @@
+package hello;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class ResponseTemplatesTest {
+    @Test
+    void responseTemplatesPage() {
+        var response = RestAssured
+                .given().log().all()
+                .when().get("/template?name=Brie")
+                .then().log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.asString()).contains("Hello, Brie!");
+    }
+}
