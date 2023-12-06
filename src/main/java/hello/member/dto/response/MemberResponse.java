69401843167ecc171ec324d1e942ed40efbce89f
sososo0 <sh0000@pusan.ac.kr>
2023-12-06 09:23:34 +0900

    feat: Add get member response dto

--- src/main/java/hello/member/dto/response/MemberResponse.java
+++ src/main/java/hello/member/dto/response/MemberResponse.java
@@ -2,6 +2,9 @@ package hello.member.dto.response;
 
 import hello.member.domain.Member;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MemberResponse {
 
     public record createMemberResponseDto(
@@ -15,6 +18,28 @@ public class MemberResponse {
                     member.getName(),
                     member.getAge()
             );
+        }
+    }
+
+    public record GetMemberResponseDto(
+            List<MemberDto> members
+    ) {
+        public static GetMemberResponseDto of(List<Member> members) {
+            return new GetMemberResponseDto(members.stream().map(MemberDto::new).collect(Collectors.toList()));
+        }
+
+        public record MemberDto(
+                Long id,
+                String name,
+                int age
+        ) {
+            public MemberDto(Member member) {
+                this(
+                        member.getId(),
+                        member.getName(),
+                        member.getAge()
+                );
+            }
         }
     }
 
